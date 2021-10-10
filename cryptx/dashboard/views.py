23b4b9fc647de0167f69cdcdea7b99118b0dd9ad
taru2001 <guptaurat2001@gmail.com>
2021-10-10 15:08:28 +0530

    dashboard created

--- cryptx/dashboard/views.py
+++ cryptx/dashboard/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
-# Create your views here.
+#Auth and messages
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from uuid import uuid4
+
+def dashboard(request):
+    return render(request, 'dashboard/dash.html')
