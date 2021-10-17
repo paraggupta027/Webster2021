02757802c3e2c6d0b4ea9c69ce1c05dc586759f7
ShobhitAgarwal <shobhitagarwal151@gmail.com>
2021-10-17 23:11:35 +0530

    coin model updated

--- cryptx/coins/models.py
+++ cryptx/coins/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Coin(models.Model):
     name = models.CharField(unique=True,max_length=30,default="")
     symbol = models.CharField(max_length=30,default="")
-    real_time_url = models.URLField(verbose_name="REAL TIME URL")
 
     def __str__(self):
        return self.name + " " + self.symbol
