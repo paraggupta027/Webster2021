33da8fe11d372ddbdaed2b734db744df0b575312
ShobhitAgarwal <shobhitagarwal151@gmail.com>
2021-10-18 00:00:41 +0530

    watchlist model added

--- cryptx/coins/models.py
+++ cryptx/coins/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
 
 class Coin(models.Model):
     name = models.CharField(unique=True,max_length=30,default="")
---
+++ cryptx/watchlist/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect, HttpResponseRedirect, reverse
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+#Auth and messages
+# from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from uuid import uuid4
+
+import json
+
+from coins.models import Coin
+
+
+
