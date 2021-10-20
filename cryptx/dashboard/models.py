3a68ad140879b18e474645e552160772907143ab
ShobhitAgarwal <shobhitagarwal151@gmail.com>
2021-10-20 22:37:03 +0530

    delete coin from watchlist icon

--- cryptx/dashboard/models.py
+++ cryptx/dashboard/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 from django.contrib.auth import get_user_model
+from django.db.models.fields import NullBooleanField
 from coins.models import Coin
 
 User=get_user_model()
+
+class Profile(models.Model):
+    email = models.CharField(max_length=50,default="")
+    # user = models.OneToOneField(User,on_delete = models.CASCADE,null=True)
+
+    def __str__(self):
+       return self.email
+
+
 
 
