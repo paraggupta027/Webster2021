785b38d6948fda587c97bab78fd029610d08ec45
taru2001 <guptaurat2001@gmail.com>
2021-10-21 11:39:21 +0530

    pull

--- cryptx/watchlist/urls.py
+++ cryptx/watchlist/urls.py
@@ -7,7 +7,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('',views.watchlist,name='watchlist'),
+    path('add_coin',views.add_coin, name = 'add_coin'),
     path('create_watchlist/',views.create_watchlist,name='create_watchlist'),
-    path('<str:watchlist>/',views.see_watchlist,name='see_watchlist'),
-    path('add_coin',views.add_coin, name = 'add_coin')
+    path('<str:watchlist>/',views.see_watchlist,name='see_watchlist')
 ]
