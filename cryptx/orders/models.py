69432486f41a30d1142f80205dce5363759eb679
paraggupta027 <paraggupta027@gmail.com>
2021-12-17 14:41:40 +0530

    PortFolio v0.1

--- cryptx/orders/models.py
+++ cryptx/orders/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.db.models.fields import NullBooleanField
 from coins.models import Coin
 from dashboard.models import Profile
-
+from portfolio.models import Portfolio
 from orders.coin_price_api import get_coin_price
 
 User=get_user_model()
@@ -34,7 +34,7 @@ class Order(models.Model):
     def can_be_executed(cls,user,coin_symbol,quantity,mode):
         cur_user = Profile.objects.get(email=user.email)
         cur_coin_price = get_coin_price(coin_symbol)
-
+        coin_obj = Coin.objects.get(symbol=coin_symbol)
         total_price = cur_coin_price*quantity
         print(total_price)
 
@@ -49,7 +49,7 @@ class Order(models.Model):
                 cur_user.save()
 
         if mode is cls.SELL:
-            coin_quantity = cls.get_quantity(user=user,coin_symbol=coin_symbol)
+            coin_quantity = Portfolio.get_quantity(user=user,coin=coin_obj)
             if coin_quantity < quantity: 
                 print(f"Not Enough {coin_quantity}")
                 return False ,f'Not enough {coin_symbol} only {coin_quantity} are available.'
@@ -59,17 +59,11 @@ class Order(models.Model):
 
 
         # Save new order in DB
-        coin_obj = Coin.objects.get(symbol=coin_symbol)
+        
 
         new_order = Order(user=user,quantity=quantity,coin=coin_obj,order_price=cur_coin_price,mode=mode)
         new_order.save()
         return True
-
-
-    @classmethod
-    def get_quantity(cls,user,coin_symbol):
-        #TODO: get real quantity of coin w.r.t user from database
-        return 2.1
-
+        
     def _str_(self):
         return self.user.email + " , order id: " + str(self.id)
