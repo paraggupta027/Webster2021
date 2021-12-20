085eaa5129c1a29137daab20acb11615215f365d
taru2001 <guptaurat2001@gmail.com>
2021-12-20 15:25:58 +0530

    toast added

--- cryptx/assets/js/buy_sell_handler.js
+++ cryptx/assets/js/buy_sell_handler.js
@@ -2,6 +2,15 @@
 let socket;
 const MARKET=1,LIMIT=2;
 
+function show_toast(x) {
+    $('.toast').toast('show');
+    document.getElementById("toast_header").innerHTML = x;
+}
+
+function dispose_toast() {
+    $('.toast').toast('dispose');
+}
+
 $(document).ready(()=>{ 
     
     socket=io("http://localhost:5000",{query:`email=${email}`});
@@ -38,10 +47,10 @@ $(document).ready(()=>{
               console.log(`${order_id} is executed`)
             },
             error: function (data) {
-                alert('An error occurred.');
+                show_toast('An error occurred.');
             },
         });
-          alert(msg);
+          show_toast(msg);
       })
 
 
@@ -83,14 +92,17 @@ $(document).ready(()=>{
                 let order = data.order;
                 if(order.order_type==MARKET || data.success==0)
                 {
-                    alert(data.msg);
+                    // alert(data.msg);
+                    let x = data.msg;
+                    show_toast(x);
                     return;
                 }
                 
                 socket.emit('schedule_buy_limit_order',order);                
             },
             error: function (data) {
-                alert('An error occurred.');
+                // alert('An error occurred.');
+                show_toast("An error occured.");
             },
         });
     })
@@ -106,10 +118,10 @@ $(document).ready(()=>{
             url: url,
             data: form.serialize(),
             success: function (data) {
-                alert(data.msg);
+                show_toast(data.msg);
             },
             error: function (data) {
-                alert('An error occurred.');
+                show_toast('An error occurred.');
             },
         });
     })
