7d5d2487a58eed0d29b38595579cd55522d50c9d
paraggupta027 <paraggupta027@gmail.com>
2021-12-20 15:40:48 +0530

    mergeing

--- cryptx/assets/js/portfolio_handler.js
+++ cryptx/assets/js/portfolio_handler.js
@@ -2,6 +2,7 @@ let portfolio=JSON.parse(document.getElementById('portfolio').textContent);
 
 console.log(portfolio)
 
+
 $(document).ready(function(){
     setInterval(() => {
         changeAllPrices();
@@ -26,19 +27,22 @@ async function changeEachCoinPrice(holding) {
     let current_price = data.USD;
     let quantity = holding.quantity
     let avg_price = holding.avg_price
-    let total_pl = (avg_price*quantity-current_price*quantity).toFixed(2)
+    let total_pl = (current_price*quantity-avg_price*quantity).toFixed(2)
     let pl_id = "id_p&l_"+coin_symbol;
     let pl_container = document.getElementById(pl_id);
+   
 
     if(total_pl>0)
     {
-        pl_container.innerHTML="+"+ total_pl;
+        pl_container.innerHTML="+"+ total_pl+'<span style="font-size:20px; font-weight:900;">&#8593;</span>';
         pl_container.style.color="green";
+       
     }
     else if(total_pl<0)
     {
-        pl_container.innerHTML=total_pl;
+        pl_container.innerHTML=total_pl+'<span style="font-size:20px; font-weight:900;">&#8595;</span>';
         pl_container.style.color="red";
+        
         // 
     }
 }
