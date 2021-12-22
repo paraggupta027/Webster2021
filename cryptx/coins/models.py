7c902498bed45f7010235e3f45910f594dc62e4c
paraggupta027 <paraggupta027@gmail.com>
2021-12-22 21:18:09 +0530

    coin added

--- cryptx/coins/models.py
+++ cryptx/coins/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth import get_user_model
 
+from orders.coin_price_api import get_coin_price
+
 User=get_user_model()
 
 class Coin(models.Model):
@@ -13,3 +15,42 @@ class Coin(models.Model):
        return f"{self.name}({self.symbol})"
     class Meta:
         verbose_name_plural = "Coin"
+
+    @classmethod
+    def add_coin(cls,name,symbol):
+        name.upper()
+        obj = cls.objects.filter(name=name)
+        if len(obj):
+            return False,f'{name} is already in database'
+        obj = cls.objects.create(name=name,symbol=symbol)
+        obj.save()
+        return True,f'{name} is added to coin database'
+
+    @classmethod
+    def remove_coin(cls,name,symbol):
+        obj = cls.objects.filter(symbol=symbol,name=name)
+        if obj:
+            obj.delete()
+            return True,f'{name} is delete from coin database'
+        return False,'No Such Coin is Present'
+
+    @classmethod 
+    def clean_coin_without_image(cls):
+        coin_list = cls.objects.all()
+        counter = 0
+        print(coin_list)
+        for coin in coin_list:
+            symbol = coin.symbol
+            coin.save()
+            
+            try:
+                f = open(f'dashboard/static/dashboard/img/SVG/{symbol}.svg','r')   
+            except:
+                coin.delete()
+            
+
+
+    def save(self, *args,**kwargs):
+        self.name = self.name.upper()
+        self.symbol = self.symbol.upper()
+        super(Coin,self).save(*args,**kwargs)
--- cryptx/orders/coin_price_api.py
+++ cryptx/orders/coin_price_api.py
@@ -1,8 +1,8 @@
 from urllib.request import urlopen
   
 import json
-
-def get_coin_price(coin_symbol):
+# from coins.models import Coin
+def  get_coin_price(coin_symbol):
 
     url ="https://min-api.cryptocompare.com/data/price?fsym=" +  coin_symbol  +"&tsyms="+"USD"
     response = urlopen(url)
@@ -14,3 +14,15 @@ def get_coin_price(coin_symbol):
     # print the json response
     print(coin_symbol +": "+str(data_json['USD']))
     return data_json['USD']
+
+def get_coins():
+    return False
+    f = open ('orders/data.json', "r")
+ 
+    # Reading from file
+    data = json.loads(f.read())
+    
+    for coin in data:
+        Coin.add_coin(name=data[coin],symbol=coin)
+        print(coin,data[coin])
+    return False
